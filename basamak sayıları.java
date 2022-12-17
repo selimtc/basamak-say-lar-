399fa522c30e338ffc620096cd5d1f749721d073
selimtc <112793794+selimtc@users.noreply.github.com>
2022-12-17 16:51:09 +0300

    Add files via upload

---
+++ basamak sayıları.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number, basamak, total=0;
+
+        System.out.print(" Sayı Giriniz: ");
+        number = input.nextInt();
+
+        while (number != 0 ){
+            basamak = number % 10;
+            total += basamak;
+            number /= 10;
+        }
+        System.out.print("Basamak Sayıları Toplamı: " + total);
+    }
+}
